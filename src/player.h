c41fe2ec96950ae7769307fd12f878f2f3dc9f06
Ravachol <rf@proton.me>
2023-06-16 12:30:25 +0200

    -version should show if there's a later version

--- src/player.h
+++ src/player.h
@@ -1,10 +1,17 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
 #include "albumart.h"
 #include "term.h"
 #include "printfunc.h"
 #include "visuals.h"
 #include "playlist.h"
+
+extern const char VERSION[];
 
 extern bool coverEnabled;
 extern bool coverBlocks;
@@ -13,3 +20,11 @@ extern int visualizationHeight;
 extern bool refresh;
 
 int printPlayer(const char *songFilepath, const char *tagsFilePath, double elapsedSeconds, double songDurationSeconds, PlayList *playlist);
+
+int fetchLatestVersion(int* major, int* minor, int* patch);
+
+void showVersion();
+
+void showHelp();
+
+#endif
