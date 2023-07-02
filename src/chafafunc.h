a8a9770a14bcbd2a60e8abac636c578182d199b2
Ravachol <rf@proton.me>
2023-07-02 02:19:37 +0200

    chafa include file

--- src/chafafunc.h
+++ src/chafafunc.h
@@ -1,4 +1,5 @@
 #include <chafa.h>
+#include <chafa-canvas-config.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdbool.h>
@@ -7,11 +8,7 @@
 #include <FreeImage.h>
 
 void printImage(const char *image_path, int width, int height);
-
 FIBITMAP *getBitmap(const char *image_path);
-
 void printBitmap(FIBITMAP *bitmap, int width, int height);
-
 void printBitmapCentered(FIBITMAP *bitmap, int width, int height);
-
 int getCoverColor(FIBITMAP* bitmap, unsigned char** r, unsigned char** g, unsigned char** b);
