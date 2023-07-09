5c51d93333a9e44d311c5308275e461478efa8de
Ravachol <rf@proton.me>
2023-07-09 09:08:56 +0200

    visualizer error

--- src/songloader.c
+++ src/songloader.c
@@ -110,8 +110,8 @@ void unloadSongData(SongData **songdata)
     if (existsFile(data->pcmFilePath))
         deleteFile(data->pcmFilePath);  
 
-    if (existsFile(data->coverArtPath))
-        deleteFile(data->coverArtPath);
+//    if (existsFile(data->coverArtPath))
+//        deleteFile(data->coverArtPath);
 
     if (data->pcmFile != NULL)
         free(data->pcmFile);
