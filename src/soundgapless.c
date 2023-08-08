8474be0a81e113901b969b599db3e6a9312766ba
Ravachol <rf@proton.me>
2023-08-08 18:37:40 +0100

    don't go over 100% sound

--- src/soundgapless.c
+++ src/soundgapless.c
@@ -491,19 +491,56 @@ double getDuration(const char *filepath)
     return duration;
 }
 
+int getCurrentVolume()
+{
+    FILE *fp;
+    char command_str[1000];
+    char buf[100];
+    int currentVolume = -1;
+
+    // Build the command string
+    snprintf(command_str, sizeof(command_str),
+             "pactl get-sink-volume @DEFAULT_SINK@ | awk 'NR==1{print $5}'");
+
+    // Execute the command and read the output
+    fp = popen(command_str, "r");
+    if (fp != NULL) {
+        if (fgets(buf, sizeof(buf), fp) != NULL) {
+            sscanf(buf, "%d", &currentVolume);
+        }
+        pclose(fp);
+    }
+
+    return currentVolume;
+}
+
 int adjustVolumePercent(int volumeChange)
 {
     char command_str[1000];
-    if (volumeChange > 0)
-        snprintf(command_str, 1000, "pactl set-sink-volume @DEFAULT_SINK@ +%d%%", volumeChange);
-    else
-        snprintf(command_str, 1000, "pactl set-sink-volume @DEFAULT_SINK@ -%d%%", -volumeChange);
-    FILE *fp = popen(command_str, "r");
+    FILE *fp;
+    int currentVolume = getCurrentVolume();
+
+    int newVolume = currentVolume + volumeChange;
+
+    // Limit new volume to a maximum of 100%
+    if (newVolume > 100)
+    {
+        newVolume = 100;
+    }
+    else if (newVolume < 0)
+    {
+        newVolume = 0;
+    }
+
+    snprintf(command_str, 1000, "pactl set-sink-volume @DEFAULT_SINK@ %d%%", newVolume);
+
+    fp = popen(command_str, "r");
     if (fp == NULL)
     {
         return -1;
     }
     pclose(fp);
+
     return 0;
 }
 
