4bc3e456a37966b1c4870406cb21489e733d7a46
Ravachol <ravacholf@proton.me>
2023-11-29 01:42:25 +0100

    version

--- src/player.c
+++ src/player.c
@@ -23,7 +23,7 @@ typedef struct
 } PixelData;
 #endif
 
-const char VERSION[] = "1.8";
+const char VERSION[] = "1.8.1";
 const int LOGO_COLOR = 3;
 const int VERSION_COLOR = 2;
 const int ABSOLUTE_MIN_WIDTH = 38;
