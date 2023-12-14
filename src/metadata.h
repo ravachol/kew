2b61ed5d14347b7e3ce47097d62759472a693401
Ravachol <ravacholf@proton.me>
2023-12-14 07:03:12 +0100

    changed include path to libavformat

--- src/metadata.h
+++ src/metadata.h
@@ -1,7 +1,6 @@
 #ifndef METADATA_H
 #define METADATA_H
-#include <libavformat/avformat.h>
-#include <libavutil/avutil.h>
+#include <avformat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
