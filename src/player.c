56f91f7c0ddc76cf88ad9e993a1604f9d1f70fa9
Ravachol <ravacholf@proton.me>
2024-09-21 18:26:40 +0200

    bump to 2.8.1

--- src/player.c
+++ src/player.c
@@ -22,7 +22,7 @@ typedef struct
 } PixelData;
 #endif
 
-const char VERSION[] = "2.8";
+const char VERSION[] = "2.8.1";
 const int ABSOLUTE_MIN_WIDTH = 68;
 bool visualizerEnabled = true;
 bool coverEnabled = true;
