334c028743abdc9f694dc8ca4556a9ff47e2d93b
Ravachol <ravacholf@proton.me>
2024-10-15 22:38:33 +0200

    version

--- src/player.c
+++ src/player.c
@@ -22,7 +22,7 @@ typedef struct
 } PixelData;
 #endif
 
-const char VERSION[] = "3.0 BETA";
+const char VERSION[] = "3.0.0-rc1";
 const int ABSOLUTE_MIN_WIDTH = 68;
 bool visualizerEnabled = true;
 bool coverEnabled = true;
