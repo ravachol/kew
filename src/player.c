ca1a455f2f861fd70fc6c30d054886fb21b3e93d
Ravachol <ravacholf@proton.me>
2024-12-27 12:35:22 +0100

    version

--- src/player.c
+++ src/player.c
@@ -15,7 +15,7 @@ Functions related to printing the player to the screen.
 #define METADATA_MAX_SIZE 256
 #endif
 
-const char VERSION[] = "3.0.2";
+const char VERSION[] = "3.0.3";
 const int ABSOLUTE_MIN_WIDTH = 68;
 bool timeEnabled = true;
 bool drewCover = true;
