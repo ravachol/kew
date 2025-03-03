2dbbe460b211f6b15db060cb27bd26d3b2433b01
Ravachol <ravacholf@proton.me>
2025-03-03 15:29:13 +0100

    prevent mouse click support

--- src/kew.c
+++ src/kew.c
@@ -214,13 +214,13 @@ struct Event processInput()
                 }
 
                 // Received mouse input instead of keyboard input
-                if (keyMappings[i].seq[0] != '\0' && strnlen(seq, MAX_SEQ_LEN) > 3 && strncmp(seq, "\033[M", 3) == 0 &&
-                   ((strncmp(seq + 1, keyMappings[i].seq, 3) == 0) ||
-                     strncmp(seq, keyMappings[i].seq, 3) == 0))
-                {
-                        event.type = keyMappings[i].eventType;
-                        break;
-                }
+                // if (keyMappings[i].seq[0] != '\0' && strnlen(seq, MAX_SEQ_LEN) > 3 && strncmp(seq, "\033[M", 3) == 0 &&
+                //    ((strncmp(seq + 1, keyMappings[i].seq, 3) == 0) ||
+                //      strncmp(seq, keyMappings[i].seq, 3) == 0))
+                // {
+                //         event.type = keyMappings[i].eventType;
+                //         break;
+                // }
         }
 
         // Handle gg
@@ -365,7 +365,8 @@ void determineSongAndNotify(UISettings *ui)
 
         bool isDeleted = determineCurrentSongData(&currentSongData);
 
-        currentSong->song.duration = currentSongData->duration;
+        if (currentSongData && currentSong)
+                currentSong->song.duration = currentSongData->duration;
 
         if (lastNotifiedId != currentSong->id)
         {
