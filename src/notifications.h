a1165adb7561befe230aaa1fd80d316569dca33d
werdahias <werdahias@riseup.net>
2025-03-09 00:18:13 +0000

    notifications.h: Define PATH_MAX in case it's undefined

--- src/notifications.h
+++ src/notifications.h
@@ -9,6 +9,10 @@
 #include <sys/time.h>
 #include "appstate.h"
 
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
+
 #ifdef USE_DBUS
 
 int displaySongNotification(const char *artist, const char *title, const char *cover, UISettings *ui);
