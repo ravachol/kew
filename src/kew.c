0eb3cb5c9dda7cf9ace0d660bf8977d8766421c2
Chromium Oxide <Chromium3Oxide@protonmail.com>
2025-03-21 19:12:00 -0400

    Add option to hide glimmering text

--- src/kew.c
+++ src/kew.c
@@ -1590,6 +1590,7 @@ void initState(AppState *state)
         state->uiSettings.hideLogo = false;
         state->uiSettings.hideHelp = false;
         state->uiSettings.quitAfterStopping = false;
+        state->uiSettings.hideGlimmeringText = false;
         state->uiSettings.coverAnsi = false;
         state->uiSettings.visualizerHeight = 5;
         state->uiSettings.titleDelay = 9;
