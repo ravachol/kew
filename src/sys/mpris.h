2f46ca331abccf754ab3e68d07eb8e3e56b082df
ravachol <rf@proton.me>
2026-02-12 03:42:38 +0100

    add doxygen style comments to sys module

--- src/sys/mpris.h
+++ src/sys/mpris.h
@@ -15,14 +15,99 @@
 
 #include <gio/gio.h>
 
+/**
+ * @brief Initializes MPRIS integration and sets up the D-Bus interfaces.
+ *
+ * This function initializes the MPRIS integration by setting up the necessary
+ * D-Bus interfaces that external clients can use to control media playback.
+ * It registers the media player with the D-Bus and prepares it for receiving
+ * commands and emitting signals related to playback.
+ */
 void init_mpris(void);
+
+/**
+ * @brief Emits a D-Bus signal to notify a change in a string property.
+ *
+ * This function emits a signal indicating that the specified string property
+ * has changed. The change is broadcast to external clients listening for
+ * D-Bus property changes.
+ *
+ * @param property_name The name of the property that has changed.
+ * @param new_value The new value of the property.
+ */
 void emit_string_property_changed(const gchar *property_name, const gchar *new_value);
+
+/**
+ * @brief Emits a D-Bus signal to notify a change in a boolean property.
+ *
+ * This function emits a signal indicating that the specified boolean property
+ * has changed. The change is broadcast to external clients listening for
+ * D-Bus property changes.
+ *
+ * @param property_name The name of the property that has changed.
+ * @param new_value The new boolean value of the property.
+ */
 void emit_boolean_property_changed(const gchar *property_name, gboolean new_value);
+
+/**
+ * @brief Emits a D-Bus signal when the volume has changed.
+ *
+ * This function emits a signal indicating that the media player's volume has
+ * changed. External clients monitoring the media player's volume can receive
+ * this signal.
+ */
 void emit_volume_changed(void);
+
+/**
+ * @brief Emits a D-Bus signal when the shuffle state has changed.
+ *
+ * This function emits a signal indicating that the shuffle state of the media
+ * player has changed (e.g., shuffle enabled or disabled). External clients
+ * can use this signal to update their interface accordingly.
+ */
 void emit_shuffle_changed(void);
+
+/**
+ * @brief Emits a D-Bus signal when metadata has changed.
+ *
+ * This function emits a signal to notify external clients that the metadata
+ * (e.g., title, artist, album, cover art, track ID) of the current media track
+ * has changed. It also provides the current song and track length.
+ *
+ * @param title The new title of the track.
+ * @param artist The new artist of the track.
+ * @param album The new album of the track.
+ * @param cover_art_path The path to the cover art image for the track.
+ * @param track_id The unique track ID.
+ * @param current_song The current song being played.
+ * @param length The length of the track in milliseconds.
+ */
 void emit_metadata_changed(const gchar *title, const gchar *artist, const gchar *album, const gchar *cover_art_path, const gchar *track_id, Node *current_song, gint64 length);
+
+/**
+ * @brief Emits a D-Bus signal when playback has started.
+ *
+ * This function emits a signal indicating that playback has started. It notifies
+ * external clients that the media player has begun playing a track.
+ */
 void emit_start_playing_mpris(void);
+
+/**
+ * @brief Emits a D-Bus signal when playback has stopped.
+ *
+ * This function emits a signal indicating that playback has stopped. It notifies
+ * external clients that the media player has ceased playing any track.
+ */
 void emit_playback_stopped_mpris(void);
+
+/**
+ * @brief Cleans up MPRIS integration and D-Bus connections.
+ *
+ * This function cleans up the MPRIS integration by unregistering D-Bus
+ * interfaces and releasing any resources associated with MPRIS functionality.
+ * It is called when the media player is shutting down or when MPRIS support
+ * is no longer needed.
+ */
 void cleanup_mpris(void);
 
 #endif
--- src/sys/notifications.h
+++ src/sys/notifications.h
@@ -17,7 +17,37 @@
 
 #ifdef USE_DBUS
 
+/**
+ * @brief Displays a notification for the current song being played.
+ *
+ * This function displays a notification with information about the current song,
+ * including the artist name, song title, and album cover (if available).
+ * It also ensures that notifications are only shown if allowed by the user and
+ * if notifications are supported by the system.
+ *
+ * The function will sanitize the artist and title strings to remove any blacklisted
+ * characters and will check if the cover image path is valid. It subscribes to
+ * notification closed signals and handles notification cleanup if necessary.
+ *
+ * @param artist The name of the artist for the song.
+ * @param title The title of the song.
+ * @param cover The file path to the album cover image.
+ * @param ui A pointer to the `UISettings` structure containing user interface preferences.
+ *
+ * @return 0 if the notification was displayed successfully, or -1 if there was an error.
+ */
 int display_song_notification(const char *artist, const char *title, const char *cover, UISettings *ui);
+
+/**
+ * @brief Cleans up and unsubscribes from notification-related resources.
+ *
+ * This function cleans up any active notifications and unsubscribes from
+ * the notification signals to prevent memory leaks. It also releases the D-Bus
+ * connection used for notifications.
+ *
+ * It ensures that the system is no longer receiving or handling notifications
+ * once the player is no longer active.
+ */
 void cleanup_notifications(void);
 
 #endif
