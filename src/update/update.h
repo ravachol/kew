5b15562ccc18ff79d9dc9dce06a6d9a0eb2faa38
ravachol <rf@proton.me>
2026-06-24 14:56:55 +0200

    don't switch to library at the end of a list unless you are on track view

--- src/update/update.h
+++ src/update/update.h
@@ -12,3 +12,5 @@
  * @return An UpdateResult that contains eventual commands to run after the update.
  */
 UpdateResult update(Model *model, struct Msg *msg);
+
+void switch_view(ViewState view_to_show);
