0f778c6956cd9553a8c2b6698350dccf4efd925d
ravachol <ravachol@noreply.codeberg.org>
2026-06-25 17:35:33 +0200

    path_max

--- src/common/path_max.h
+++ src/common/path_max.h
@@ -1,23 +1,31 @@
 #include <limits.h>
 
+#ifndef KEW_PATH_MAX
+
 #ifdef _WIN32
+
+// Windows APIs can support longer paths with Unicode APIs.
+// This is an internal buffer size, not the MAX_PATH limit.
 #define KEW_PATH_MAX 32768
+
 #else
 
-#ifndef PATH_MAX
-// Internal application path buffer size.
-// This is not the Windows MAX_PATH limit.
-// Windows Unicode APIs can handle paths longer than 260 characters.
+#ifdef PATH_MAX
+
+#define KEW_PATH_MAX PATH_MAX
+
+#else
+
+// Fallback internal buffer size when PATH_MAX is unavailable.
 #if defined(__APPLE__)
 #define KEW_PATH_MAX 1024
 #else
 #define KEW_PATH_MAX 4096
 #endif
 
-#else
-#define KEW_PATH_MAX PATH_MAX
-#endif
+#endif /* PATH_MAX */
 
-#endif
+#endif /* _WIN32 */
 
+#endif /* KEW_PATH_MAX */
 
