4d6a09e01ec480d86a82fdf73bdfda4f7b904f3c
ravachol <ravachol@noreply.codeberg.org>
2026-07-08 18:13:57 +0200

    fix build error

--- src/kew.c
+++ src/kew.c
@@ -76,6 +76,7 @@ OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
 #include "ops/track_manager.h"
 
 #include "data/theme.h"
+#include "data/img_func.h"
 
 #include "utils/file.h"
 #include "utils/term.h"
