5c0116b0ae8d419b374ac9076946d7906e3a25ad
ravachol <ravachol@noreply.codeberg.org>
2026-08-27 16:29:15 +0200

    tinywebp: remove warning. fixes #268

--- include/tinywebp/tiny_webp.h
+++ include/tinywebp/tiny_webp.h
@@ -178,23 +178,23 @@
 
   Copyright (c) 2010, 2011, Google Inc. All rights reserved.
   Copyright (c) 2025, justus2510
-  
+
   Redistribution and use in source and binary forms, with or without
   modification, are permitted provided that the following conditions are
   met:
-  
+
     * Redistributions of source code must retain the above copyright
       notice, this list of conditions and the following disclaimer.
-  
+
     * Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in
       the documentation and/or other materials provided with the
       distribution.
-  
+
     * Neither the name of Google nor the names of its contributors may
       be used to endorse or promote products derived from this software
       without specific prior written permission.
-  
+
   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -206,7 +206,7 @@
   THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
   OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  
+
 */
 
 
@@ -4662,7 +4662,14 @@ static unsigned char *twp__read_entire_file(const char *file_path, int *size)
     *size = ftell(f);
     fseek(f, 0, SEEK_SET);
     void *mem = malloc(*size);
-    fread(mem, 1, *size, f);
+    size_t nread = fread(mem, 1, *size, f);
+
+    // Check added by ravachol for kew
+    if (nread != (size_t)*size) {
+        free(mem);
+        fclose(f);
+        return NULL;
+    }
     fclose(f);
 
     return (unsigned char *)mem;
