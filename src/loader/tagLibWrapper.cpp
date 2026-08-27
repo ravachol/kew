972956816c066e434f69a98d222336918ee11b58
ravachol <ravachol@noreply.codeberg.org>
2026-08-27 07:50:01 +0200

    taglibwrapper, remove warning

--- src/loader/tagLibWrapper.cpp
+++ src/loader/tagLibWrapper.cpp
@@ -244,6 +244,48 @@ std::string toLower(const std::string &str)
         return lowerStr;
 }
 
+bool looksLikeJpeg(const std::vector<unsigned char> &data)
+{
+        return data.size() > 4 && data[0] == 0xFF && data[1] == 0xD8 &&
+               data[data.size() - 2] == 0xFF &&
+               data[data.size() - 1] == 0xD9;
+}
+bool looksLikePng(const std::vector<unsigned char> &data)
+{
+        static const unsigned char pngHeader[8] = {
+            0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+        if (data.size() < 12)
+                return false;
+        if (memcmp(data.data(), pngHeader, 8) != 0)
+                return false;
+        // Look for IEND within last 16 bytes
+        for (size_t i = data.size() >= 16 ? data.size() - 16 : 0;
+             i + 3 < data.size(); ++i) {
+                if (memcmp(&data[i], "IEND", 4) == 0)
+                        return true;
+        }
+        return false;
+}
+bool looksLikeWebp(const std::vector<unsigned char> &data)
+{
+        return data.size() > 12 && memcmp(&data[0], "RIFF", 4) == 0 &&
+               memcmp(&data[8], "WEBP", 4) == 0;
+}
+
+std::string getImageExtension(const std::vector<unsigned char> &data)
+{
+        if (looksLikeJpeg(data))
+                return ".jpg";
+
+        if (looksLikePng(data))
+                return ".png";
+
+        if (looksLikeWebp(data))
+                return ".webp";
+
+        return ".jpg";
+}
+
 extern "C" {
 #include "utils/utils.h"
 
@@ -325,48 +367,6 @@ void parseFlacPictureBlock(const std::vector<unsigned char> &data,
                 return;
 
         imageData.assign(&ptr[offset], &ptr[offset + dataLength]);
-}
-
-bool looksLikeJpeg(const std::vector<unsigned char> &data)
-{
-        return data.size() > 4 && data[0] == 0xFF && data[1] == 0xD8 &&
-               data[data.size() - 2] == 0xFF &&
-               data[data.size() - 1] == 0xD9;
-}
-bool looksLikePng(const std::vector<unsigned char> &data)
-{
-        static const unsigned char pngHeader[8] = {
-            0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
-        if (data.size() < 12)
-                return false;
-        if (memcmp(data.data(), pngHeader, 8) != 0)
-                return false;
-        // Look for IEND within last 16 bytes
-        for (size_t i = data.size() >= 16 ? data.size() - 16 : 0;
-             i + 3 < data.size(); ++i) {
-                if (memcmp(&data[i], "IEND", 4) == 0)
-                        return true;
-        }
-        return false;
-}
-bool looksLikeWebp(const std::vector<unsigned char> &data)
-{
-        return data.size() > 12 && memcmp(&data[0], "RIFF", 4) == 0 &&
-               memcmp(&data[8], "WEBP", 4) == 0;
-}
-
-std::string getImageExtension(const std::vector<unsigned char> &data)
-{
-        if (looksLikeJpeg(data))
-                return ".jpg";
-
-        if (looksLikePng(data))
-                return ".png";
-
-        if (looksLikeWebp(data))
-                return ".webp";
-
-        return ".jpg";
 }
 
 bool withImageExtension(std::string &filename,
