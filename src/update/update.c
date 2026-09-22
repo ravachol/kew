0093c8f9ef351cc85b38aa6dfcd53e76b102826b
ravachol <ravachol@noreply.codeberg.org>
2026-09-22 09:14:30 +0200

    Make periodic refresh, refresh only relevant parts of the screen to avoid glitch on ghostty #304

--- src/update/update.c
+++ src/update/update.c
@@ -410,7 +410,10 @@ void refresh_if_timeout(void)
         if (difftime(now, refresh_set_time) >= PERIODICAL_REFRESH_TIMEOUT_SECONDS)
         {
                 if (model->state.currentView != TRACK_VIEW)
-                        set_dirty(DIRTY_ALL);
+                        set_dirty(DIRTY_PROGRESS | DIRTY_LIBRARY | DIRTY_PLAYLIST | DIRTY_SEARCH);
+
+                // FIXME: introduce a variable DIRTY_HELP so we can update the help texts too
+
                 refresh_set_time = now;
         }
 }
